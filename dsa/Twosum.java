d9190a90ea928433f4077355bfb4aa9aa259df76
KirubakaranM2006 <kirubakaranm3007@gmail.com>
2026-07-19 12:05:09 +0530

    array based question

---
+++ dsa/Twosum.java
@@ -0,0 +1,20 @@
+package dsa;
+
+public class Twosum {
+
+	public static void main(String[] args) {
+		int arr[]= {1,4,3,5,6};
+		int target=4;
+		for(int i=0;i<arr.length;i++) {
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i]+arr[j]==target) {
+					System.out.println(i+":"+j);
+					return;
+					
+				}
+			}
+		}
+
+	}
+
+}
