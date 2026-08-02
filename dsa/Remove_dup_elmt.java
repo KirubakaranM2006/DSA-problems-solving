89097c5190df7b27bfc8b5ff375883975c5e5d31
KirubakaranM2006 <kirubakaranm3007@gmail.com>
2026-08-03 04:51:56 +0530

    array based question

---
+++ dsa/Remove_dup_elmt.java
@@ -0,0 +1,28 @@
+package dsa_TCS_NQT;
+
+import java.util.Scanner;
+
+public class Remove_dup_elmt {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int N=scan.nextInt();
+		int arr[]=new int[N];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=scan.nextInt();
+		}
+		int visitedalready=-1;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]!=-1) {
+				for(int j=i+1;j<arr.length;j++) {
+					if(arr[i]==arr[j]) {
+						arr[j]=visitedalready;
+					}
+				}System.out.println(arr[i]);
+			}
+			
+		}
+
+	}
+
+}
